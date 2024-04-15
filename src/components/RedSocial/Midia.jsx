ac85b8fdc481f468cb454a42a9b3c048f857138c
pedro imbrech <elprogramadorpedro@gmail.com>
2024-04-15 18:13:51 -0300

    feat:midia

---
+++ src/components/RedSocial/Midia.jsx
@@ -0,0 +1,14 @@
+// En alguna parte de tu aplicación, por ejemplo en una página de producto
+import SocialMediaIntegration from './components/SocialMediaIntegration';
+
+function ProductPage() {
+    return (
+        <div>
+            <h1>Product Name</h1>
+            <p>Description of the product.</p>
+            <SocialMediaIntegration />
+        </div>
+    );
+}
+
+export default ProductPage;
